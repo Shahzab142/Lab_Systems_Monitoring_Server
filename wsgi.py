d7477e5ac49798b2f5124113bc8e20edbfff7e4b
Shahzaib Munawar <shahzaibmunawar920@gmail.com>
2026-02-14 19:55:03 +0500

    update server code

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+from gevent import monkey
+monkey.patch_all()
+
+from app import create_app
+from app.extensions import socketio
+
+app = create_app()
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host="0.0.0.0", port=5050)
